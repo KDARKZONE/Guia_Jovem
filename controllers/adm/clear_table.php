cfa55bec57290adbda051332c54d92c358b94636
Kauã Almeida <kauaalmeida8995@gmail.com>
2023-05-16 14:42:59 -0300

    reboot

--- controllers/adm/clear_table.php
+++
@@ -1,8 +0,0 @@
-<?php
-   if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    if(!empty($_POST['Esvaziar'])){
-        $sql = "DELETE FROM perfis";
-        $stmt = $BANCO_DE_DADOS->exec($sql);
-    }
-   }
-?>
