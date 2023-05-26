389b69302c298735fc07ff38ad27a2fa57198047
AlunoETC <alunoetc@EDUC.NET>
2023-05-26 10:51:54 -0300

    reset

--- app/views/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    # para trabalhar com sessões sempre iniciamos com session_start.
-    session_start();
-    # destroi todas sessões, se existirem.
-    session_destroy();
-
-    # redireciona para a pagina inicial.
-    header('location: index.php');
-?>
