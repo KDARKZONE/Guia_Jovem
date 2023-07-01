67d5d4786679f5d8212bd8ef03acfe27c9d1a296
Kaua <kauaalmeida8995@gmail.com>
2023-07-01 10:46:00 -0300

    ggpx

--- app/views/adm/controllers/delete_user.php
+++ app/views/adm/controllers/delete_user.php
@@ -38,8 +38,7 @@
         $stmt = $db->prepare($sql);
         $stmt->bindParam(':id',$id);
         $stmt->execute();
-        header("Location:dados.php");
+        header("Location:../dados.php");
         exit;
     }
-    var_dump($stmtDeletePerfil);
 ?>
--- app/views/resul-search.php
+++ app/views/resul-search.php
@@ -10,7 +10,7 @@
         $sql = "SELECT * FROM post";
         $stmt = $db->prepare($sql);
         $stmt->execute();
-          $caminho = '../autor/controllers/';
+          $caminho = 'autor/controllers/';
      @$Letra = $_POST['search'];
             $sql_curso = "SELECT*FROM post WHERE categoria = :categoria";
             $stmt_curso = $db->prepare($sql_curso);
@@ -20,19 +20,19 @@
                 ?>
                 <article>
               <div class="noticias">
-                <a href="../artigo_show.php?id=<?= $row['ID_post'] ?>">
+                <a href="artigo_show.php?id=<?= $row['ID_post'] ?>">
                   <img src="<?= $caminho . $row["thumb"]; ?>" alt="Imagem post" title="Imagem Post" style="width:300px;height:300px;">
                 </a>
                 <p>
-                  <a href="../artigo_show.php?id=<?= $row['categoria'] ?>" class="category"><?= $row['categoria'] ?></a>
+                  <a href="artigo_show.php?id=<?= $row['categoria'] ?>" class="category"><?= $row['categoria'] ?></a>
                 </p>
                 <h2>
-                  <a href="../artigo_show.php?id=<?= $row['ID_post'] ?>" class="title">
+                  <a href="artigo_show.php?id=<?= $row['ID_post'] ?>" class="title">
                     <?=$row['titulo']?>
                   </a>
                 </h2>
                 <h4>
-                <a href="../artigo_show.php?id=<?= $row['conteudo'] ?>" class="category"><?= $row['conteudo'] ?></a>
+                <a href="artigo_show.php?id=<?= $row['conteudo'] ?>" class="category"><?= $row['conteudo'] ?></a>
                 </h4>
               </div>
             </article>
